4af39c5d859de62b1ba7c321ccbe0e365a98929c
Manu Pacheco <mpachecopal@gmail.com>
2020-03-27 12:35:03 +0100

    add mutations examples

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const gqlMiddleware = require('express-graphql')
 // para poder leer el schema y poder separar
 const { readFileSync } = require('fs')
 const { join } = require('path')
-const reslovers = require('./lib/resolvers')
+const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.HOST || 3000
 
 // definicion del esquema
 const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
-const schema = makeExecutableSchema({ typeDefs, reslovers })
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 app.use('/api', gqlMiddleware({
   schema: schema,
-  rootValue: reslovers.Query,
+  rootValue: resolvers,
   graphiql: true
 }))
 
