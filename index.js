05f26860db3b34fcd6f28d2456ec90a62d46d1a5
Manu Pacheco <mpachecopal@gmail.com>
2020-04-12 12:53:28 +0200

    add enums, interfaces, directives, unions

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 require('dotenv').config()
 const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
+const cors = require('cors')
 const gqlMiddleware = require('express-graphql')
 // para poder leer el schema y poder separar
 const { readFileSync } = require('fs')
@@ -15,6 +16,8 @@ const port = process.env.HOST || 3000
 // definicion del esquema
 const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
 const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+app.use(cors())
 
 app.use('/api', gqlMiddleware({
   schema: schema,
